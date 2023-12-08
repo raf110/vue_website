2edeb26284c23218563dcb63da2411f6d62ceffc
raf110 <113583945+raf110@users.noreply.github.com>
2023-12-08 11:57:35 +0100

    Add files via upload

---
+++ src/views/LoginView.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="login">
+        <LoginForm/>
+    </div>       
+  </template>
+
+  <script>
+  import LoginForm from "../components/LoginComp.vue";
+
+  export default {
+    components: {
+      LoginForm  
+    }
+  }
+  </script>
+
+<style>
+body {
+  background:#fefefe;
+}
+.login {
+  margin: 2em;
+  display: flex;
+  
+}
+
+.nav-tabs .nav-link {
+  color: #729cee!important;
+}
+
+.nav-tabs .nav-link.active, .nav-tabs .nav-item.show .nav-link {
+  color: #142F64!important;
+}
+
+
+</style>
